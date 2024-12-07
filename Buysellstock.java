fb878a93c9d001e8fca0dc9a6537ef786991694a
Deepak858 <deepaknayak9857@gmail.com>
2024-12-07 20:35:08 +0530

    Add files via upload

---
+++ Buysellstock.java
@@ -0,0 +1,33 @@
+public class Buysellstock {
+    public static int main(int []price) {
+        int buyprice=Integer.MAX_VALUE;
+        int maxprofit=0;
+        for(int i=0;i<price.length;i++){
+            if(buyprice<price[i]){
+                int profit=price[i]-buyprice;
+                maxprofit=Math.max(maxprofit, profit);
+            }
+            else
+          buyprice=  price[i];
+        }
+        return maxprofit;
+    //  int buyprice=Integer.MAX_VALUE;
+    //  int maxprofit=0;
+    //  for(int i=0;i<price.length;i++){
+    //     if(buyprice<price[i]){
+    //         int profit=price[i]-buyprice;
+    //         maxprofit=Math.max(profit, maxprofit);
+         
+    //     }
+    //     maxprofit=buyprice;
+    //  }
+    //  return  maxprofit;
+
+    }
+    public static void main(String[] args) {
+        int price[]={7,1,5,3,6,4};
+        System.out.println(main(price));
+
+    }
+    
+}
